394da87949122a151b4560443c380286fdef867b
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2023-08-21 18:28:56 -0400

    d

--- Screener.py
+++ Screener.py
@@ -198,6 +198,5 @@ class Screener:
 		elif type == 'intraday': return get_intraday(browser)
 
 if __name__ == '__main__':
-	Screener.run(datetime.datetime(2023,8,21,9,15))
 	Screener.run('current')
 	study.run(study,True)
